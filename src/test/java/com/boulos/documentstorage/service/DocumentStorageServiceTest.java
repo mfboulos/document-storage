5a00321dfd7a52d15e74ab29b6f069938b5a4686
mfboulos <mboulos95@gmail.com>
2019-08-08 02:19:29 -0400

    Few tests, some written ahead of time

--- src/test/java/com/boulos/documentstorage/service/DocumentStorageServiceTest.java
+++ src/test/java/com/boulos/documentstorage/service/DocumentStorageServiceTest.java
@@ -1,64 +1,113 @@
 package com.boulos.documentstorage.service;
 
+import org.apache.commons.lang.StringUtils;
 import org.h2.engine.Database;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.boulos.documentstorage.config.AppConfig;
+import com.boulos.documentstorage.database.DocumentRepository;
 import com.boulos.documentstorage.exception.DocumentNotFoundException;
+import com.boulos.documentstorage.model.DocumentMetadata;
 
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DocumentStorageServiceTest {
 	@Mock
-	Database database;
+	private DocumentRepository testRepo;
 	@Mock
-	AppConfig appConfig;
+	private AppConfig appConfig;
+	@Mock
+	private MultipartFile testFile;
+	@Mock
+	private InputStream mockStream;
+	@Spy
+	private DocumentStorageService testDSS;
 	
-	DocumentStorageService testDSS;
+	private final Path testPath = Paths.get(".");
 	
 	@Before
-	public void setUp() {
-		when(appConfig.getDirectory()).thenReturn(".");
+	public void setUp() throws DocumentNotFoundException, IOException {
+		when(testRepo.findById(any())).thenReturn(Optional.empty());
+		when(testRepo.findById("25isfunnierthan24lol")).thenReturn(
+				Optional.of(new DocumentMetadata("25isfunnierthan24lol", 1000L, "jpg")));
+		when(testFile.getOriginalFilename()).thenReturn("cool_dog.jpg");
+		
+		mockStream = mock(InputStream.class);
+		when(mockStream.read(any())).thenReturn(0);
+		when(testFile.getInputStream()).thenReturn(mockStream);
+		
+		testDSS.setRepo(testRepo);
+		testDSS.setStorageDirectory(testPath);
 	}
 	
 	@Test(expected=DocumentNotFoundException.class)
 	public void testLoadThrowsExceptionIfGivenInvalidDocId() throws DocumentNotFoundException {
-		throw new DocumentNotFoundException();
+		testDSS.load("testid12320character");
 	}
 	
 	@Test
-	public void testLoadReturnsDocumentIfGivenValidDocId() throws DocumentNotFoundException {
+	public void testLoadReturnsResourceIfGivenValidDocId() throws DocumentNotFoundException {
+		Resource testResource = mock(Resource.class);
+		when(testResource.exists()).thenReturn(true);
+		when(testResource.isReadable()).thenReturn(true);
+		when(testResource.getDescription()).thenReturn("Cool stuff");
+		doReturn(testResource).when(testDSS)
+				.loadFromPath(testPath.resolve("25isfunnierthan24lol.jpg"));
+		Resource result = testDSS.load("25isfunnierthan24lol");
 		
+		assertEquals(testResource.getDescription(), result.getDescription());
 	}
 
 	@Test
-	public void testDocumentIsStoredFromMultipartForm() {
-		
+	public void testDocumentIsStoredFromMultipartForm() throws IOException {
+		testDSS.store(testFile);
+		verify(testRepo).save(any());
+		verify(mockStream).read(any());
 	}
 
 	@Test(expected=DocumentNotFoundException.class)
 	public void testUpdateThrowsExceptionIfGivenInvalidDocId() throws DocumentNotFoundException {
-		throw new DocumentNotFoundException();
+		testDSS.update("testid12320character", testFile);
 	}
 
 	@Test
-	public void testUpdateOverwritesDocumentWithValidDocId() throws DocumentNotFoundException {
-		
+	public void testUpdateOverwritesDocumentWithValidDocId() throws IOException {
+		testDSS.update("25isfunnierthan24lol", testFile);
+		verify(testRepo).save(any());
+		verify(mockStream).read(any());
 	}
 	
 	@Test(expected=DocumentNotFoundException.class)
 	public void testDeleteThrowsExceptionIfGivenInvalidDocId() throws DocumentNotFoundException {
-		throw new DocumentNotFoundException();
+		testDSS.delete("testid12320character");
 	}
 	
 	@Test
 	public void testDocumentIsDeletedIfGivenValidDocId() throws DocumentNotFoundException {
-		
+		testDSS.delete("25isfunnierthan24lol");
+		verify(testRepo).delete(any());
+		// verify file is deleted
+	}
+	
+	@Test
+	public void testGeneratedIdsAreValid() {
+		String id = testDSS.generateId();
+		assertEquals(20, id.length());
+		assertTrue(StringUtils.isAlphanumeric(id));
 	}
 }
