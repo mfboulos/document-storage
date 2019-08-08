a22f63f5446d9750953a2ed2ed2a9c450a5ff3dc
mfboulos <mboulos95@gmail.com>
2019-08-07 20:06:51 -0400

    Configuration binding

--- src/main/java/com/boulos/documentstorage/DocumentStorageWebApp.java
+++ src/main/java/com/boulos/documentstorage/DocumentStorageWebApp.java
@@ -1,9 +1,15 @@
 package com.boulos.documentstorage;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.boulos.documentstorage.config.AppConfig;
+
+@SpringBootApplication
+@EnableConfigurationProperties(AppConfig.class)
 public class DocumentStorageWebApp extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
---
+++ src/main/java/com/boulos/documentstorage/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.boulos.documentstorage.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@PropertySource("classpath:application.yml")
+@ConfigurationProperties(prefix="storage")
+@Getter
+@Setter
+public class AppConfig {
+	private String directory;
+}
